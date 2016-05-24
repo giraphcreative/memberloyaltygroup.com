961202924cd82b5e6ad2d05c8e9778b96f40d285
James Pederson <james@jpederson.com>
2016-05-24 00:12:13 -0500

    Update title bar site name.

--- library/title.php
+++ library/title.php
@@ -3,10 +3,12 @@
 
 function the_large_title() {
 
+	$slides = get_post_meta( get_the_ID(), CMB_PREFIX . "showcase", 1 );
+
 	// get the slides
 	if ( has_cmb_value( 'large-title' ) ) {
 		?>
-		<div class="large-title wrap">
+		<div class="large-title wrap <?php print ( !empty( $slides ) ? 'has-showcase' : '' ); ?>">
 			<h1><?php show_cmb_value( 'large-title' ) ?></h1>
 			<?php if ( has_cmb_value( 'large-title-subtitle' ) ) { ?>
 			<h3><?php show_cmb_value( 'large-title-subtitle' ) ?></h3>
