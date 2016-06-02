5d396da38800a2b563db86e00b92ef9606aab14f
James Pederson <james@jpederson.com>
2016-06-02 16:51:50 -0500

    Add pagination and fix some styling on two column blog page.

--- index.php
+++ index.php
@@ -11,30 +11,30 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 
-			<div class="content-narrow blog-list">
-			<?php
-			if ( is_search() ) {
-				?><h1>Search Results for <span>'<?php print $_REQUEST["s"]; ?>'</span></h1><?php
-			}
+		<div class="content-narrow blog-list">
+		<?php
+		if ( is_search() ) {
+			?><h1>Search Results for <span>'<?php print $_REQUEST["s"]; ?>'</span></h1><?php
+		}
 
-			while ( have_posts() ) : the_post();
-				?>
-				<div class="post">
-					<?php the_post_thumbnail() ?>
-					<div class="post-content">
-						<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-						<?php the_excerpt(); ?>
-						<p><a href="<?php the_permalink() ?>">read more ></a></p>
-					</div>
-				</div>
-				<?php
-			endwhile;
-
+		while ( have_posts() ) : the_post();
 			?>
+			<div class="post">
+				<?php the_post_thumbnail() ?>
+				<div class="post-content">
+					<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+					<?php the_excerpt(); ?>
+					<p><a href="<?php the_permalink() ?>">read more ></a></p>
+				</div>
 			</div>
-			<div class="pagination">
-				<?php pagination(); ?>
-			</div>
+			<?php
+		endwhile;
+
+		?>
+		</div>
+		<div class="pagination">
+			<?php pagination(); ?>
+		</div>
 
 	</div><!-- #primary -->
 
